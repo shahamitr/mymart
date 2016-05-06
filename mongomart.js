190750cff23da51052d71415f64016aa0e961f10
Amit Shah <amit.shah@infostretch.com>
2016-05-06 14:07:50 +0530

    Added connection string

--- mongomart.js
+++ mongomart.js
@@ -51,7 +51,10 @@ var ITEMS_PER_PAGE = 5;
 // Hardcoded USERID for use with the shopping cart portion
 var USERID = "558098a65133816958968d88";
 
-MongoClient.connect('mongodb://localhost:27017/mongomart', function(err, db) {
+var connectionString = "mongodb://shahamitr:shahamitr@ds015962.mlab.com:15962/mongomart";
+//var connectionString = "'mongodb://localhost:27017/mongomart";
+
+MongoClient.connect(connectionString, function(err, db) {
     "use strict";
 
     assert.equal(null, err);
